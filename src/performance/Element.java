a375d847e70cce6287123afb5e7dd44fc4e004f9
yangfan <yfsmx89@163.com>
2016-11-03 23:40:22 +0800

    first version

---
+++ src/performance/Element.java
@@ -0,0 +1,8 @@
+package performance;
+
+public class Element {
+	private Object[] objArray;
+	public Element() {
+		objArray = new Object[10000000];
+	}
+}
---
+++ src/performance/Main.java
@@ -0,0 +1,27 @@
+package performance;
+
+//分页中断
+public class Main {
+	final static int MAX_ROWS = 1000;
+	final static int MAX_COLUMNS = 10000;
+	final static Element BLANK_VALUE = new Element();
+	public static void main(String[] args) {
+		Element[][] table = new Element[MAX_ROWS][MAX_COLUMNS];
+		
+		long startTime = System.currentTimeMillis();
+		//bad
+		for (int i = 0; i < MAX_COLUMNS; i++) {
+			for (int j = 0; j < MAX_ROWS; j++) {
+				table[j][i] = BLANK_VALUE;
+			}
+		}
+		//good
+		for (int i = 0; i < MAX_ROWS; i++) {
+			for (int j = 0; j < MAX_COLUMNS; j++) {
+				table[i][j] = BLANK_VALUE;
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+	}
+}
