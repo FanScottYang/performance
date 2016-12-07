b5085ed0e312eff8518ff538a2b03f83edbc97dc
Yang Fan <fan.yang@rea-group.com>
2016-12-07 22:57:41 +0800

    add recursion method

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -8,22 +8,30 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-//        System.out.println(calculate1(40));
-        System.out.println(calculate2(10));
+        System.out.println(calculate1(35));
         long endTime = System.currentTimeMillis();
+        System.out.println("Total time usage: " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        System.out.println(calculate2(35));
+        endTime = System.currentTimeMillis();
+        System.out.println("Total time usage: " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        System.out.println(calculate3(35));
+        endTime = System.currentTimeMillis();
         System.out.println("Total time usage: " + (endTime - startTime));
     }
 
     private static int calculate1(int a) {
-        if (a == 1) {
+        if (a == 1 || a == 2) {
             return 1;
-        } else if (a == 2) {
-            return 2;
         } else {
             return calculate1(a - 1) + calculate1(a - 2);
         }
     }
 
+    //用栈消除递归
     private static int calculate2(int a) {
         int result = 0;
         Stack<int[]> stack = new Stack<>();
@@ -31,25 +39,49 @@ public class Main {
         stack.add(f);
         while (!stack.isEmpty()) {
             int[] t = stack.peek();
-            if (t[0] == 1) {
+            if (t[0] == 1 || t[0] == 2) {
+                result = 1;
                 stack.pop();
-                stack.peek()[1] ++;
-                stack.peek()[2] += 1;
-            } else if (t[0] == 2) {
-                stack.pop();
-                stack.peek()[1] ++;
-                stack.peek()[2] += 2;
+                if (!stack.isEmpty()) {
+                    stack.peek()[1]++;
+                    stack.peek()[2]++;
+                }
             } else if (t[1] == -2) {
-                int[] tmp = {a-1, -2, 0};
+                int[] tmp = {t[0] - 1, -2, 0};
                 stack.add(tmp);
             } else if (t[1] == -1) {
-                int[] tmp = {a-2, -2, 0};
+                int[] tmp = {t[0] - 2, -2, 0};
                 stack.add(tmp);
             } else if (t[1] == 0) {
                 int tmp = stack.pop()[2];
-                stack.peek()[2] += tmp;
+                result = tmp;
+                if (!stack.isEmpty()) {
+                    stack.peek()[1]++;
+                    stack.peek()[2] += tmp;
+                }
             }
         }
+        return result;
+    }
+
+    //用循环消除尾递归和单向递归
+    private static int calculate3(int a) {
+        int f1, f2, f3 = 0;
+        if (a == 1 || a == 2) {
+            return 1;
+        }
+        f1 = 1;
+        f2 = 1;
+        for (int i = 3; i <= a; i++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+
+    //大问题由相同的小问题组成(数学归纳法), open -> closed
+    private static int calculate4(int a) {
         return 0;
     }
 }
